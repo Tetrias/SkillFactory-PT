2c282159c2cad1d54598979b36b2f7195fa1e885
Tetrias <pr1ir870@gmail.com>
2022-08-24 20:53:31 +0700

    Update TicTacToe.py
    Косметические изменения, добавления комментариев.

--- TicTacToe.py
+++ TicTacToe.py
@@ -4,55 +4,57 @@ import random
 import sys
 
 
-def getGameBoard():
-    board = []
+def getGameField():
+    # Получить игровое поле:
+    field = []
     for i in range(3):
-        board.append(['[ ]', '[ ]', '[ ]'])
-    return board
+        field.append(['[ ]', '[ ]', '[ ]'])
+    return field
 
 
-def drawGameBoard(board):
+def drawGameField(field):
+    # Нарисовать игровое поле:
     print('   1  2  3')
     for y in range(3):
         print(f'{y + 1} ', end='')
         for x in range(3):
-            print(board[x][y], end='')
+            print(field[x][y], end='')
         print()
 
 
-def isWinner(board, pSign):
-    return ((board[0][0] == pSign and board[0][1] == pSign and board[0][2] == pSign) or
-            (board[1][0] == pSign and board[1][1] == pSign and board[1][2] == pSign) or
-            (board[2][0] == pSign and board[2][1] == pSign and board[2][2] == pSign) or
-            (board[0][0] == pSign and board[1][0] == pSign and board[2][0] == pSign) or
-            (board[0][1] == pSign and board[1][1] == pSign and board[2][1] == pSign) or
-            (board[0][2] == pSign and board[1][2] == pSign and board[2][2] == pSign) or
-            (board[0][0] == pSign and board[1][1] == pSign and board[2][2] == pSign) or
-            (board[0][2] == pSign and board[1][1] == pSign and board[2][0] == pSign))
+def isWinner(field, pSign):
+    # Проверить победителя:
+    return ((field[0][0] == pSign and field[0][1] == pSign and field[0][2] == pSign) or
+            (field[1][0] == pSign and field[1][1] == pSign and field[1][2] == pSign) or
+            (field[2][0] == pSign and field[2][1] == pSign and field[2][2] == pSign) or
+            (field[0][0] == pSign and field[1][0] == pSign and field[2][0] == pSign) or
+            (field[0][1] == pSign and field[1][1] == pSign and field[2][1] == pSign) or
+            (field[0][2] == pSign and field[1][2] == pSign and field[2][2] == pSign) or
+            (field[0][0] == pSign and field[1][1] == pSign and field[2][2] == pSign) or
+            (field[0][2] == pSign and field[1][1] == pSign and field[2][0] == pSign))
 
 
-def isDraw(board):
+def isDraw(field):
+    # Проверить возможность продолжать игру:
     pSign = '[ ]'
-    return ((board[0][0] == pSign and board[0][1] == pSign and board[0][2] == pSign) or
-            (board[1][0] == pSign and board[1][1] == pSign and board[1][2] == pSign) or
-            (board[2][0] == pSign and board[2][1] == pSign and board[2][2] == pSign) or
-            (board[0][0] == pSign and board[1][0] == pSign and board[2][0] == pSign) or
-            (board[0][1] == pSign and board[1][1] == pSign and board[2][1] == pSign) or
-            (board[0][2] == pSign and board[1][2] == pSign and board[2][2] == pSign) or
-            (board[0][0] == pSign and board[1][1] == pSign and board[2][2] == pSign) or
-            (board[0][2] == pSign and board[1][1] == pSign and board[2][0] == pSign))
+    return ((field[0][0] == pSign and field[0][1] == pSign and field[0][2] == pSign) or
+            (field[1][0] == pSign and field[1][1] == pSign and field[1][2] == pSign) or
+            (field[2][0] == pSign and field[2][1] == pSign and field[2][2] == pSign) or
+            (field[0][0] == pSign and field[1][0] == pSign and field[2][0] == pSign) or
+            (field[0][1] == pSign and field[1][1] == pSign and field[2][1] == pSign) or
+            (field[0][2] == pSign and field[1][2] == pSign and field[2][2] == pSign) or
+            (field[0][0] == pSign and field[1][1] == pSign and field[2][2] == pSign) or
+            (field[0][2] == pSign and field[1][1] == pSign and field[2][0] == pSign))
 
 
-def isOnBoard(x, y):
-    return 0 <= x <= 2 and 0 <= y <= 2
-
-
-def isValidMove(board, xmove, ymove):
-    if board[xmove][ymove] != '[ ]' or not isOnBoard(xmove, ymove):
+def isValidMove(field, xmove, ymove):
+    # Проверить возможность хода:
+    if field[xmove][ymove] != '[ ]' or not (0 <= xmove <= 2 and 0 <= ymove <= 2):
         return False
 
 
 def getPlayerSign():
+    # Распределение знаков между игроками и первого хода:
     sign = ''
     while not (sign == '[X]' or sign == '[O]'):
         randomizer = random.randint(0, 1)
@@ -66,7 +68,8 @@ def getPlayerSign():
         return ['[O]', '[X]', 'Игрок 2']
 
 
-def getMove(board):
+def getMove(field):
+    # Получение и проверка хода игрока:
     DIGITS1TO3 = '1 2 3'.split()
     while True:
         print('Введите ход (например "11") или "выход", что бы завершить игру.')
@@ -76,7 +79,7 @@ def getMove(board):
         if len(move) == 2 and move[0] in DIGITS1TO3 and move[1] in DIGITS1TO3:
             x = int(move[0]) - 1
             y = int(move[1]) - 1
-            if isValidMove(board, x, y) == False:
+            if not isValidMove(field, x, y):
                 continue
             else:
                 break
@@ -86,50 +89,59 @@ def getMove(board):
 
 
 def playGame(player1Sign, player2Sign, turn):
+    # Основная функция для игры:
     print(turn + ' играет за "Х" и ходит первым.')
-    board = getGameBoard()
+    field = getGameField()
 
     while True:
-        stillPlaying = isDraw(board)
+        # Проверка возможности продолжать игру:
+        stillPlaying = isDraw(field)
         if not stillPlaying:
-            return board, 'draw'
+            return field, 'draw'
 
+        # Ход первого игрока:
         elif turn == 'Игрок 1':
-            drawGameBoard(board)
-            if isWinner(board, player2Sign):
-                return board, 'Игрок 2'
+            drawGameField(field)
+            # Проверить не победил ли второй игрок на прошлом ходу, перед ходом первого игрока:
+            if isWinner(field, player2Sign):
+                return field, 'Игрок 2'
             print('Ход "Игрока 1"')
-            move = getMove(board)
+            move = getMove(field)
+            # Проверить если игрок хочет выйти, если нет, завершить ход:
             if move == 'выход':
                 print('Благодарим за игру!')
                 sys.exit()
             else:
-                board[move[0]][move[1]] = player1Sign
+                field[move[0]][move[1]] = player1Sign
             turn = 'Игрок 2'
 
+        # Ход второго игрока
         elif turn == 'Игрок 2':
-            drawGameBoard(board)
-            if isWinner(board, player1Sign):
-                return board, 'Игрок 1'
+            drawGameField(field)
+            # Проверить не победил ли первый игрок на прошлом ходу, перед ходом второго игрока:
+            if isWinner(field, player1Sign):
+                return field, 'Игрок 1'
             print('Ход "Игрока 2"')
-            move = getMove(board)
+            move = getMove(field)
+            # Проверить если игрок хочет выйти, если нет, завершить ход:
             if move == 'выход':
                 print('Благодарим за игру!')
                 sys.exit()
             else:
-                board[move[0]][move[1]] = player2Sign
+                field[move[0]][move[1]] = player2Sign
             turn = 'Игрок 1'
 
 
 print('Добро пожаловать в игру "Крестики-нолики"')
 
 while True:
+    # Основной код в виде цикла, для возможности повтора игры по завершению.
     player1Sign, player2Sign, turn = getPlayerSign()
     while True:
+        gameField, result = playGame(player1Sign, player2Sign, turn)
+        drawGameField(gameField)
 
-        gameBoard, result = playGame(player1Sign, player2Sign, turn)
-        drawGameBoard(gameBoard)
-
+        # Вывод основных результатов.
         if result == 'Игрок 1':
             print('"Игрок 1" победил!')
             break
@@ -141,7 +153,7 @@ while True:
         else:
             print('Ничья!')
             break
-
+    # Предложение повторить игру.
     print('Хотите сыграть еще раз? (да или нет)')
     if not input().lower() == 'да':
         break
