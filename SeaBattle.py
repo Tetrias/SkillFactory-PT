ae96d1ca1f5c7663a817c5415fd9876426179c89
Tetrias <pr1or870@gmail.com>
2022-09-03 05:15:56 +0700

    Update SeaBattle.py
    Переделал с нуля.

--- SeaBattle.py
+++ SeaBattle.py
@@ -3,6 +3,7 @@
 from sys import exit
 from itertools import product
 from random import choice
+from random import randint
 
 
 # Классы исключений, для отлова и отображения сообщений пользователю.
@@ -38,6 +39,7 @@ class CheckMove:
         self.r = user_input[2:3]
         self.length = len(user_input)
         self.r_digits = (2, 4, 6, 8)
+        self.moves = all_moves()
 
     # Проверяем всевозможные варианты некорректного ввода и отлавливаем соответсвующее им исключение.
     def check_exception(self):
@@ -48,122 +50,157 @@ class CheckMove:
                 raise DigitsException
             elif int(self.x) <= 0 or int(self.x) <= 0 or int(self.x) > 6 or int(self.x) > 6:
                 raise BoardOutException
+            elif (int(self.x), int(self.y)) not in self.moves:
+                raise BoardUsedException
         except BoardException as er:
             print(er)
         else:
             return True
 
 
-# Класс для хранения объектов рисования доски.
-class Dots:
-    empty_cell = '0'
-    ship_cell = '■'
-    destroyed_ship = 'X'
-    damaged_ship = '□'
-    miss_cell = '•'
-    field_size = 6
+def all_moves():
+    row, col, moves = [], [], []
+    for x in range(field_size):
+        for y in range(field_size):
+            row.append(x)
+            col.append(y)
+            moves = list(product(row, col))
+    return list(set(moves))
 
 
-# Класс для отрисовки поля и изменения в нем.
-class Field:
-    def __init__(self):
-        self.size = Game.field_size
-        self.board = [[Dots.empty_cell for _ in range(self.size)] for _ in range(self.size)]
-
-    @property
-    def all_moves(self):
-        row, col, moves = [], [], []
-        for x in range(self.size):
-            for y in range(self.size):
-                row.append(x)
-                col.append(y)
-                moves = list(product(row, col))
-        return list(set(moves))
-
-    def __str__(self):
-        board_res = ''
-        board_res += "  | 1 | 2 | 3 | 4 | 5 | 6 |"
-        for i, row in enumerate(self.board):
-            board_res += f"\n{i + 1} | " + " | ".join(row) + " |"
-        # if current_player != 'Человек':
-        #     board_res = board_res.replace(Dots.ship_cell, Dots.empty_cell)
-        # return board_res
+def get_field():
+    field = [[empty_cell for _ in range(field_size)] for _ in range(field_size)]
+    return field
 
 
-# Класс для взаимодействия с кораблями.
-class Ship:
+def draw_field(field, player):
+    field_res = ''
+    field_res += "  | 1 | 2 | 3 | 4 | 5 | 6 |"
+    for i, row in enumerate(field):
+        field_res += f"\n{i + 1} | " + " | ".join(row) + " |"
+    if player:
+        field_res = field_res.replace(ship_cell, empty_cell)
+    return field_res
+
+
+def cycle_for_getting_board():
+    field = get_field()
+    while True:
+        r = random_ship(field)
+        if r:
+            return field, r
+
+
+def is_space_free():
     pass
 
 
-# Класс для логики игроков.
-class Player:
-    def __init__(self):
-        self.player = "" #'Человек'
-        self.available_move = Field().all_moves
+def random_ship(board):
+    moves = all_moves()
+    counter = 0
+    ships = []
+    for i in ship_size:
+        while True:
+            counter += 1
+            if counter > 2000:
+                return False
+            cell = choice(moves)
+            if board[cell[0]][cell[1]] == '0':
+                if i == 1:
+                    ships.append(cell)
+                    break
+                else:
+                    rotation = rotate_ship(i, board, cell)
+                    if rotation:
+                        ships.append(rotation)
+                        break
+    return ships
 
-    def get_input(self):
-        if self.player == 'Человек':
-            print('Сделайте ход.')
-            print('Подсказка: нужно ввести два числа, где первое - ряд, второе - столбец.')
-            print('Или введите "выход" для завершения игры.')
-            got_move = User().get_shot_input()
-            x, y = int(got_move[0]) - 1, int(got_move[1]) - 1
+
+def rotate_ship(s_ship, board, cell):
+    cont = contour(cell, True)
+    rot_cell = []
+    for i in cont:
+        a = []
+        if board[int(i[0])][int(i[1])] == '0':
+            if s_ship == 2:
+                rot_cell.append(cell)
+                rot_cell.append(i)
+                return rot_cell
+            else:
+                a = (i[0] - cell[0], i[1] - cell[1])
+                a = (a[0] + i[0], a[1] + i[1])
+                if 0 > a[0] or a[0] >= field_size or 0 > a[1] or a[1] >= field_size:
+                    continue
+                elif board[a[0]][a[1]] == '0':
+                    print(a[0], a[1])
+                    rot_cell.append(cell)
+                    rot_cell.append(i)
+                    rot_cell.append(a)
+                    return rot_cell
+
+
+def contour(x_y, rotate=False):
+    moves = all_moves()
+    res = []
+    row, col = x_y[0], x_y[1]
+    if rotate:
+        near = [(-1, 0), (0, -1), (0, 1), (1, 0)]
+    else:
+        near = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 0), (0, 1), (1, -1), (1, 0), (1, 1)]
+    for x, y in near:
+        if 0 > (x + row) or (x + row) >= field_size or 0 > (y + col) or (y + col) >= field_size:
+            continue
         else:
-            got_move = AI().get_ai_move()
-            x, y = got_move[0], got_move[1]
-        return x, y
+            res += [(x + row, y + col)]
+    return res
 
 
-# Класс для пользователя.
-class User(Player):
-    # Получаем ход игрока и проверяем корректность ввода.
-    @staticmethod
-    def get_shot_input():
+def clean_contour(available_move, cont):
+    for i in available_move[:]:
+        if i in cont:
+            available_move.remove(i)
+            cont.remove(i)
+    return cont
+
+
+def get_input(player, available_move):
+    if player == 'Человек':
+        print('Сделайте ход.')
+        print('Подсказка: нужно ввести два числа, где первое - ряд, второе - столбец.')
+        print('Или введите "выход" для завершения игры.')
         while True:
             user_input = input().replace(" ", "")
             # Отправляем в класс проверки ходов, если метод проверки не вернул True, повторить цикл.
             check_input = CheckMove(user_input)
             valid_input = check_input.check_exception()
             if valid_input is True:
-                return user_input
+                x, y = int(user_input[0]) - 1, int(user_input[1]) - 1
+                break
+    else:
+        ai_input = choice(available_move)
+        x, y = ai_input[0], ai_input[1]
+    return x, y
 
 
-# Класс для ИИ.
-class AI(Player):
-    # Получаем ход компьютера.
-
-    def get_ai_move(self):
-        ai_input = choice(self.available_move)
-        return ai_input
-
-    # Обрисовываем контур, если предыдущий ход попал или убил корабль.
-    @staticmethod
-    def contour(x_y):
-        res = []
-        row, col = x_y[0], x_y[1]
-        near = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 0), (0, 1), (1, -1), (1, 0), (1, 1)]
-        for x, y in near:
-            res += [(x + row, y + col)]
-        return res
-
-    # Убираем клетки в которые ходить уже не нужно.
-    @staticmethod
-    def clean_contour(coord, cont):
-        for i in coord[:]:
-            if i in cont:
-                coord.remove(i)
-                cont.remove(i)
-        return coord
+def play_game():
+    player_1 = cycle_for_getting_board()
+    player_2 = cycle_for_getting_board()
+    field = player_1[0]
+    ships = player_1[1]
+    print(player_1[1])
 
 
-# Класс для логики.
-class Game:
-    field_size = 6
-    ships_size = [3, 2, 2, 1, 1, 1, 1]
-
+field_size = 6
+ship_size = [3, 2, 2, 1, 1, 1, 1]
+empty_cell = '0'
+ship_cell = '■'
+destroyed_ship = 'X'
+damaged_ship = '□'
+miss_cell = '•'
+print('Добро пожаловать в игру "Морской бой"')
+print('Подождите пока корабли встанут по местам.')
 
 while True:
-    g = Game()
-    a = Player().get_input()
-    print(a)
+    play_game()
     break
