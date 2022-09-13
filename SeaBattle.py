dc1725bad2ffd9421dda9fea3e6ca3e433f68f96
Tetrias <pr1or870@gmail.com>
2022-09-13 19:49:01 +0700

    Update SeaBattle.py

--- SeaBattle.py
+++ SeaBattle.py
@@ -39,18 +39,25 @@ class CheckMove:
 
     # Проверяем всевозможные варианты некорректного ввода и отлавливаем соответсвующее им исключение.
     def check_exception(self):
+        g = Game()
         try:
+            # Если вводимых игроком символов меньше двух.
             if self.length < 2:
                 raise LengthOutException
+            # Если вводимые игроком символы не являются числом.
             elif not self.x.isdigit() or not self.y.isdigit():
                 raise DigitsException
-            elif int(self.x) <= 0 or int(self.y) <= 0 or int(self.x) > 6 or int(self.y) > 6:
+            # Если числа не в пределах 1-6.
+            elif int(self.x) <= 0 or int(self.y) <= 0 or int(self.x) > g.board_size or int(self.y) > g.board_size:
                 raise BoardOutException
+            # Если координаты указывают на клетку не соответствующую ходу.
             elif self.board[int(self.x) - 1][int(self.y) - 1] != Game().empty_cell and \
                     self.board[int(self.x) - 1][int(self.y) - 1] != Game().ship_cell:
                 raise BoardUsedException
+        # Вывести соответствующее сообщение об ошибке.
         except BoardException as er:
             print(er)
+        # Если всё в порядке, возвращаем True.
         else:
             return True
 
@@ -64,38 +71,40 @@ class Board:
         self.ship = g.ship_cell
         self.board = None
 
-    # Функция вызывающая цикл, который повторяется до тех пор, пока корабли не будут расставлены.
+    # Метод вызывающий цикл, который повторяется до тех пор, пока корабли не будут расставлены.
     def cycle_for_getting_board(self):
         while True:
             self.board = [[self.empty for _ in range(self.size)] for _ in range(self.size)]
             board, ships = Ship().random_ship(self.board)
+            # Если доска удачно заполнена, возвращаем доску и координаты кораблей.
             if board:
                 list_merged = [item for sublist in ships[:3] for item in sublist], ships[3:]
                 ships = [item for sublist in list_merged for item in sublist]
                 return board, ships
 
-    # Функция для удаления клеток из случайного выбора расстановки кораблей.
+    # Метод для удаления клеток из случайного выбора расстановки кораблей.
     @staticmethod
     def get_free_space(length, moves, rotated):
         g = Game()
         result = []
-        if not rotated:
-            pass
-        elif length == 1:
+        # Если корабль занимает одну клетку, получаем клетки вокруг него.
+        if length == 1:
             result = (g.contour(rotated))
+        # Если клеток больше одной, получаем клетки вокруг каждой из них.
         else:
             for i in rotated:
                 result.append(g.contour(i))
+        # Вызываем функцию, удаляющую занятые клетки и клетки вокруг из списка доступных клеток.
         moves = g.clean_contour(length, moves, result)
         return moves
 
-    # Функция для замены пустых клеток на клетки корабля.
+    # Метод для замены пустых клеток на клетки корабля.
     def place_ship_on_board(self, board, rotated):
         for i in range(len(rotated)):
             for x, y in rotated:
                 board[x][y] = self.ship
 
-    # Функция для рисования доски.
+    # Метод для рисования доски.
     def draw_board(self, player):
         field_res = " "
         for i in range(self.size):
@@ -103,11 +112,12 @@ class Board:
         field_res += " |"
         for i, row in enumerate(player.board):
             field_res += f"\n{i + 1} | " + " | ".join(row) + " |"
-        # if player.name == "Компьютер":
-        #     field_res = field_res.replace(self.ship, self.empty)
+        # Если рисуем доску для Компьютера, то скрываем его корабли.
+        if player.name == "Компьютер":
+            field_res = field_res.replace(self.ship, self.empty)
         return field_res
 
-    # Функция для вывода поля.
+    # Метод для вывода поля.
     @staticmethod
     def print_board(one, two):
         print('Поле компьютера:                                                     Поле игрока:')
@@ -119,46 +129,48 @@ class Board:
         print(f'{two[140:167]}  # X - клетка уничтоженного корабля. #   {one[140:167]}')
         print(f'{two[168:]}  #####################################   {one[168:]}')
 
-    # Функция для изменения клетки поврежденного корабля, на уничтоженные.
+    # Метод для изменения клетки поврежденного корабля, на уничтоженные.
     @staticmethod
     def ship_destroyed(x, y, player, enemy):
         g = Game()
         coordinates = []
         cont = g.contour([x, y])
-
+        # Меняем клетки вокруг уничтоженной на пустые.
         for c_x, c_y in cont:
             enemy.board[x][y] = g.destroyed_ship
+            # Если клетка рядом пустая, удалим её из доступных для ИИ ходов.
             if (c_x, c_y) in player.moves and not (c_x, c_y) in enemy.ships:
                 player.moves.remove((c_x, c_y))
-            if c_x < 0 or c_x >= 6 or c_y < 0 or c_y >= 6:
+            # Если координаты вне нужного диапазона, пропускаем.
+            if c_x < 0 or c_x >= g.board_size or c_y < 0 or c_y >= g.board_size:
                 continue
-
+            # Если клетка принадлежит раненному кораблю, меняем её на уничтоженный.
             elif enemy.board[c_x][c_y] == g.damaged_ship:
                 enemy.board[c_x][c_y] = g.destroyed_ship
                 coord = g.contour((c_x, c_y))
-
+                # И создаем цикл внутри цикла, что бы очистить клетки и вокруг этой.
                 for row, col in coord:
                     if (row, col) in player.moves and not (row, col) in enemy.ships:
                         player.moves.remove((row, col))
-                    if row < 0 or row >= 6 or col < 0 or col >= 6:
+                    if row < 0 or row >= g.board_size or col < 0 or col >= g.board_size:
                         continue
-
+                    # Если найдена ещё одна клетка поврежденного корабля.
                     elif enemy.board[row][col] == g.damaged_ship:
                         enemy.board[row][col] = g.destroyed_ship
                         coordinates = [row, col]
-
+                    # Если клетка пуста, заменяем на клетку промаха.
                     elif enemy.board[row][col] == g.empty_cell:
                         enemy.board[row][col] = g.miss_cell
 
             elif enemy.board[c_x][c_y] == g.empty_cell:
                 enemy.board[c_x][c_y] = g.miss_cell
-
+        # Если есть координаты ещё одной клетки, помимо двух предыдущих, запускаем похожий цикл для неё.
         if coordinates:
             cont = g.contour(coordinates)
             for c_x, c_y in cont:
                 if (c_x, c_y) in player.moves and not (c_x, c_y) in enemy.ships:
                     player.moves.remove((c_x, c_y))
-                if c_x < 0 or c_x >= 6 or c_y < 0 or c_y >= 6:
+                if c_x < 0 or c_x >= g.board_size or c_y < 0 or c_y >= g.board_size:
                     continue
 
                 elif enemy.board[c_x][c_y] == g.damaged_ship:
@@ -173,22 +185,26 @@ class Ship:
     def __init__(self):
         self.ships = []
 
-    # Основная функция для получения координат корабля.
+    # Основной метод для получения координат корабля.
     def random_ship(self, board):
         g = Game()
         b = Board()
         moves = g.all_moves()
         for i in g.ship_pool:
             while True:
+                # Если клеток для расстановки кораблей не осталось, вернуть False.
                 if not moves:
                     return False, False
+                # Случайно выбрать из доступных клеток, куда поставить корабль.
                 else:
                     cell = choice(moves)
+                # Если размер корабля всего 1.
                 if i == 1:
                     self.ships.append(cell)
                     board[cell[0]][cell[1]] = g.ship_cell
                     moves = b.get_free_space(i, moves, cell)
                     break
+                # Если больше 1, переходим в функцию для выбора направления корабля.
                 else:
                     rotated = self.rotate_ship(i, cell, moves)
                     if rotated:
@@ -198,22 +214,25 @@ class Ship:
                         break
         return board, self.ships
 
-    # Функция для выбора направления корабля.
+    # Метод для выбора направления корабля.
     @staticmethod
     def rotate_ship(length, cell, moves):
         cont = Game().contour(cell, True)
         rotated = []
         for i in cont:
             rotated.clear()
+            # Если координаты среди доступных.
             if i in moves:
                 if length == 2:
                     rotated.append(cell)
                     rotated.append(i)
                     return rotated
+                # Если размер больше 2.
                 else:
                     a = i[0] - cell[0] + i[0], i[1] - cell[1] + i[1]
+                    # Если 3-ю клетку нельзя поставить.
                     if a not in moves:
-                        break
+                        continue
                     else:
                         rotated.append(cell)
                         rotated.append(i)
@@ -233,36 +252,42 @@ class Players:
             self.previous_move = []
             self.to_kill = False
 
-    # Функция для получения хода.
+    # Метод для получения хода.
     @staticmethod
     def get_input(player, enemy):
+        # Если игрок человек, то получаем ход, с помощью соответствующего метода.
         if player.name == 'Человек':
             x_y = User.user_move(enemy)
+        # Иначе другой метод для ИИ.
         else:
+            # Если компьютер в режиме добивания, выбираем ход из приоритетных, иначе просто из доступных.
             if player.to_kill:
                 x_y = choice(player.priority)
                 player.priority.remove(x_y)
             else:
                 x_y = choice(player.moves)
             player.moves.remove(x_y)
+
         result = Players.check_shoot(x_y, player, enemy)
         return result
 
+    # Метод проверки хода.
     @staticmethod
     def check_shoot(x_y, player, enemy):
         g = Game()
-
+        # Если координаты указывают на клетку корабля.
         if enemy.board[x_y[0]][x_y[1]] == g.ship_cell:
             cont = g.contour(x_y, True)
-
+            # По средствам цикла проверяем если рядом есть другие клетки корабля.
             for x, y in cont:
                 elem = ((x_y[0] - x), (x_y[1] - y))
                 if x < 0 or x >= g.board_size or y < 0 or y >= g.board_size:
                     continue
-
+                # Если рядом есть клетка целого корабля, возвращаем "попадание".
                 elif enemy.board[x][y] == g.ship_cell:
                     enemy.board[x_y[0]][x_y[1]] = g.damaged_ship
                     enemy.ships.remove(x_y)
+                    # Если ходит ИИ, вводим его в режим добивания.
                     if player.name == "Компьютер":
                         if player.to_kill:
                             AI.to_kill_enemy(x_y, player)
@@ -270,17 +295,18 @@ class Players:
                             player.previous_move = x_y
                             AI.to_kill_enemy(x_y, player)
                     return 'get'
-
+                # Если рядом есть клетка уничтоженного корабля, проверяем нет ли рядом с ним клетки целого корабля.
                 elif enemy.board[x][y] == g.destroyed_ship:
                     if enemy.board[x_y[0] - elem[0]][x_y[1] - elem[1]] == g.ship_cell:
                         enemy.ships.remove(x_y)
-                        if player.to_kill:
-                            AI.to_kill_enemy(x_y, player)
-                        else:
-                            player.previous_move = x_y
-                            AI.to_kill_enemy(x_y, player)
+                        if player.name == "Компьютер":
+                            if player.to_kill:
+                                AI.to_kill_enemy(x_y, player)
+                            else:
+                                player.previous_move = x_y
+                                AI.to_kill_enemy(x_y, player)
                         return 'get'
-
+            # Если рядом клеток целого корабля не осталось, возвращаем "убил".
             Board.ship_destroyed(x_y[0], x_y[1], player, enemy)
             enemy.board[x_y[0]][x_y[1]] = g.destroyed_ship
             enemy.ships.remove(x_y)
@@ -288,7 +314,7 @@ class Players:
                 player.to_kill = False
                 player.priority.clear()
             return 'kill'
-
+        # Если все условия выше ложны, возвращаем "промах"
         else:
             enemy.board[x_y[0]][x_y[1]] = g.miss_cell
             return 'miss'
@@ -296,6 +322,7 @@ class Players:
 
 # Класс пользователя.
 class User(Players):
+    # Метод для получения и проверки хода игрока.
     @staticmethod
     def user_move(enemy):
         print('Сделайте ход.')
@@ -316,15 +343,17 @@ class User(Players):
 
 # Класс ИИ.
 class AI(Players):
+    # Метод режима добивания ИИ.
     @staticmethod
     def to_kill_enemy(x_y, player):
+        # Если это первое попадание, задаем приоритет для клеток вокруг.
         if not player.to_kill:
             player.to_kill = True
             cont = Game().contour(x_y, True)
             cont = set(cont).intersection(set(player.moves))
             for i in cont:
                 player.priority.append(i)
-
+        # Иначе, очищаем приоритет от ходов, где клеток корабля точно быть не может.
         else:
             prev = player.previous_move
             cont = prev[0] - x_y[0], prev[1] - x_y[1]
@@ -348,7 +377,7 @@ class Game:
         self.damaged_ship = '□'
         self.miss_cell = '•'
 
-    # Функция для получения всевозможных ходов.
+    # Метод для получения всевозможных ходов.
     def all_moves(self):
         row, col, moves = [], [], []
         for x in range(self.board_size):
@@ -358,24 +387,25 @@ class Game:
                 moves = list(product(row, col))
         return list(set(moves))
 
-    # Функция для определения контуров хода.
+    # Метод для определения контуров клетки.
     @staticmethod
     def contour(x_y, rotate=False):
         res = []
-        if not x_y:
-            return False
         row, col = x_y[0], x_y[1]
+        # Если метод в режиме ротации выдаем клетки только по горизонтали и вертикали.
         if rotate:
             near = [(-1, 0), (0, -1), (0, 1), (1, 0)]
+        # Иначе, выдаем и клетки по углам.
         else:
             near = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 0), (0, 1), (1, -1), (1, 0), (1, 1)]
         for x, y in near:
             res += [(x + row, y + col)]
         return res
 
-    # Функция удаляющая из возможных ходов те, совершать которые нет смысла.
+    # Метод удаляющий из возможных ходов те, совершать которые нет смысла.
     @staticmethod
     def clean_contour(length, moves, cont):
+        # Если длинна корабля больше одного, распаковываем списки в списках.
         if length > 1:
             cont = [item for sublist in cont for item in sublist]
         while cont:
@@ -387,14 +417,17 @@ class Game:
                     cont.remove(i)
         return moves
 
+    # Основной метод игры.
     @staticmethod
     def playing_game(user, ai):
         b = Board()
         player, enemy = user, ai
         while True:
+            # Если текущий игрок человек, рисуем доску.
             if player.name == "Человек":
                 b.print_board(b.draw_board(user), b.draw_board(ai))
             res = Players.get_input(player, enemy)
+            # Если результат хода "промах" меняем текущего и следующего игрока местами.
             if res == 'miss':
                 print(f'{player.name} - промах!')
                 player, enemy = enemy, player
@@ -404,6 +437,7 @@ class Game:
                 continue
             elif res == 'kill' or int:
                 print(f'{player.name} - убил!')
+            # Проверяем если у следующего игрока ещё остались корабли.
             if not enemy.ships:
                 b.print_board(b.draw_board(user), b.draw_board(ai))
                 return player.name
@@ -411,11 +445,13 @@ class Game:
 
 if __name__ == "__main__":
     while True:
+        # Задаем объекты для игроков и запускаем в метод игры.
         us_board = Board().cycle_for_getting_board()
         ai_board = Board().cycle_for_getting_board()
         player1 = Players(name="Человек", board=us_board[0], ships=us_board[1])
         player2 = Players(name="Компьютер", board=ai_board[0], ships=ai_board[1])
         winner = Game().playing_game(player1, player2)
+        # Отображаем победителя и спрашиваем если игрок хочет сыграть ещё раз.
         print(f'Игра окончена!\nПобедитель - {winner}!')
         print('Хотите сыграть ещё раз? (Для повтора введите "да")')
         if not input().lower() == 'да':
