5b69302f000b32a94cd659f0f2829fc8383a0bed
Tetrias <pr1or870@gmail.com>
2022-09-18 22:58:50 +0700

    Update custom_filters.py

--- project_django/news/templatetags/custom_filters.py
+++ project_django/news/templatetags/custom_filters.py
@@ -6,7 +6,13 @@ register = template.Library()
 
 @register.filter()
 def censor(text):
-    for i in text.split():
-        if i[0].isupper():
-            text = text.replace(i, i[0] + '*' * (len(i) - 1))
+    try:
+        if type(text) is not str:
+            raise Exception('Невозможно обработать значение, так как оно не является строкой.')
+    except Exception as e:
+        print(e)
+    else:
+        for i in text.split():
+            if i[0].isupper():
+                text = text.replace(i, i[0] + '*' * (len(i) - 1))
     return text
