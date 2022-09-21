29e01dc7b33773a268661fd37376b1fc1a07b2b6
Tetrias <pr1or870@gmail.com>
2022-09-21 20:40:00 +0700

    ...

--- project_django/news/templatetags/custom_filters.py
+++ project_django/news/templatetags/custom_filters.py
@@ -16,3 +16,11 @@ def censor(text):
             if i[0].isupper():
                 text = text.replace(i, i[0] + '*' * (len(i) - 1))
     return text
+
+
+@register.simple_tag(takes_context=True)
+def url_replace(context, **kwargs):
+    d = context['request'].GET.copy()
+    for k, v in kwargs.items():
+        d[k] = v
+    return d.urlencode()
